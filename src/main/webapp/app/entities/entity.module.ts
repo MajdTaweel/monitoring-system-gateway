cfd9a0b1c220af4c124cbbe40910dc87fdda4db1
Majd Taweel <42496516+MajdTaweel@users.noreply.github.com>
2020-10-23 19:31:55 +0300

    Regenerate

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,20 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'sensing-node',
+        loadChildren: () => import('./sensingnode/sensing-node/sensing-node.module').then(m => m.SensingnodeSensingNodeModule),
+      },
+      {
+        path: 'magnetometer-readings',
+        loadChildren: () =>
+          import('./sensingnode/magnetometer-readings/magnetometer-readings.module').then(m => m.SensingnodeMagnetometerReadingsModule),
+      },
+      {
+        path: 'pollution-readings',
+        loadChildren: () =>
+          import('./sensingnode/pollution-readings/pollution-readings.module').then(m => m.SensingnodePollutionReadingsModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
